d3629d5727b0c14bfda4edcb8d6668024b1941db
osgoo <oosgoo966@gmail.com>
2025-12-22 18:50:29 +0800

    2

--- lib/screens/movies.dart
+++ lib/screens/movies.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:first_movie/model/movie/index.dart';
+import 'package:first_movie/widgets/movie_special_card.dart';
 import 'package:flutter/material.dart';
 
 class Movies extends StatefulWidget {
@@ -8,10 +12,52 @@ class Movies extends StatefulWidget {
 }
 
 class _MoviestState extends State<Movies> {
+  Future<List<MovieModel>> getData() async {
+    String res = await DefaultAssetBundle.of(
+      context,
+    ).loadString("assets/movies.json");
+    return MovieModel.fromList(jsonDecode(res));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Киноны жагсаалт"),
+    return FutureBuilder(
+      future: getData(),
+      builder: ((context, snapshot) {
+        if (snapshot.hasData) {
+          final specialData = snapshot.data!.length > 3
+              ? snapshot.data!.sublist(0, 3)
+              : snapshot.data;
+          return Column(
+            children: [
+              Text("Шилдэг кино"),
+              SizedBox(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: List.generate(
+                      specialData!.length,
+                      ((index) => MovieSpecialCard(
+                        specialData[index],
+                      )),
+                    ),
+                  ),
+                ),
+              ),
+              Text(
+                "Бүх кинонууд",
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
+          );
+        } else {
+          return Center(
+            child: SizedBox(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      }),
     );
   }
 }
--- lib/widgets/movie_special_card.dart
+++ lib/widgets/movie_special_card.dart
@@ -1,10 +1,22 @@
+import 'package:first_movie/model/movie/index.dart';
 import 'package:flutter/material.dart';
 
 class MovieSpecialCard extends StatelessWidget {
-  const MovieSpecialCard({super.key});
+  final MovieModel data;
+  const MovieSpecialCard(this.data, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double width = MediaQuery.of(context).size.width * 0.5;
+    return Container(
+      height: width,
+      width: width,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(data.imgUrl),
+        ),
+      ),
+      child: Icon(Icons.play_circle),
+    );
   }
 }
